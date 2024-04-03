aa74ab48e596ac07f967173cdf68b6533ce13107
Zach <19974370+Zfauser@users.noreply.github.com>
2024-04-02 21:48:13 -0400

    need to add more comments and play again loop. this is for thursday up until today

---
+++ colors.h
@@ -0,0 +1,43 @@
+#include <string>
+
+using namespace std;
+
+// Foregrounds
+const string BLACK = "\033[30m";
+const string RED = "\033[31m";
+const string GREEN = "\033[32m";
+const string YELLOW = "\033[33m";
+const string BLUE = "\033[34m";
+const string MAGENTA = "\033[35m";
+const string CYAN = "\033[36m";
+const string WHITE = "\033[37m";
+const string BRIGHT_BLACK = "\033[90m";
+const string BRIGHT_RED = "\033[91m";
+const string BRIGHT_GREEN = "\033[92m";
+const string BRIGHT_YELLOW = "\033[93m";
+const string BRIGHT_BLUE = "\033[94m";
+const string BRIGHT_MAGENTA = "\033[95m";
+const string BRIGHT_CYAN = "\033[96m";
+const string BRIGHT_WHITE = "\033[97m";
+
+// Backgrounds
+const string BLACK_BG = "\033[40m";
+const string RED_BG = "\033[41m";
+const string GREEN_BG = "\033[42m";
+const string YELLOW_BG = "\033[43m";
+const string BLUE_BG = "\033[44m";
+const string MAGENTA_BG = "\033[45m";
+const string CYAN_BG = "\033[46m";
+const string WHITE_BG = "\033[47m";
+const string BRIGHT_BLACK_BG = "\033[100m";
+const string BRIGHT_RED_BG = "\033[101m";
+const string BRIGHT_GREEN_BG = "\033[102m";
+const string BRIGHT_YELLOW_BG = "\033[103m";
+const string BRIGHT_BLUE_BG = "\033[104m";
+const string BRIGHT_MAGENTA_BG = "\033[105m";
+const string BRIGHT_CYAN_BG = "\033[106m";
+const string BRIGHT_WHITE_BG = "\033[107m";
+
+const string BOLD = "\033[1m";
+const string UNDERLINE = "\033[4m";
+const string RESET = "\033[0m";
